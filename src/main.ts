1bd53028da201d21366282439c96dc1f56f3dc08
Roberto Barbosa <robertojf@gmail.com>
2021-04-24 20:43:48 -0300

    Add ClassSerializerInterceptor

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,14 @@
-import { NestFactory } from '@nestjs/core';
+import { NestFactory, Reflector } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { useContainer } from 'class-validator';
 import { CustomValidationPipe } from './pipes/custom-validation.pipe';
+import { ClassSerializerInterceptor } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   useContainer(app.select(AppModule), { fallbackOnErrors: true });
   app.useGlobalPipes(new CustomValidationPipe());
+  app.useGlobalInterceptors(new ClassSerializerInterceptor(app.get(Reflector)));
   await app.listen(3000);
 }
 bootstrap();
