4bdd3794bf7b6f06a3594dae2acaef852366052d
Roberto Barbosa <robertojf@gmail.com>
2021-06-03 15:33:32 -0300

    Fix submenus

--- src/modules/users/users.service.ts
+++ src/modules/users/users.service.ts
@@ -117,7 +117,7 @@ export class UsersService {
               include: [
                 {
                   model: Modules,
-                  attributes: ['description', 'title', 'router'],
+                  attributes: ['description', 'title', 'router', 'submenus'],
                 },
               ],
               attributes: ['isShow', 'isGet', 'isPost', 'isUpdate', 'isDelete'],
