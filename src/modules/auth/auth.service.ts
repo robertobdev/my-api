c9efa932c93fb84717c26059c088f39a08e3c2a9
Roberto Barbosa <robertojf@gmail.com>
2021-06-15 21:03:41 -0300

    Add send email to auth service

--- src/modules/auth/auth.service.ts
+++ src/modules/auth/auth.service.ts
@@ -3,6 +3,7 @@ import { LoginUserDto } from '../users/dto/login-user.dto';
 import { UsersService } from '../users/users.service';
 import { JwtService } from '@nestjs/jwt';
 import { RequestPasswordDto } from '../users/dto/request-password.dto';
+import EmailController from '../../utils/helpers/mail/controller/email-controller';
 
 @Injectable()
 export class AuthService {
@@ -19,11 +20,17 @@ export class AuthService {
 
   async requestPassword(requestPassword: RequestPasswordDto) {
     const user = await this.userService.requestPassword(requestPassword);
+
     if (!user) {
       return true;
     }
-
     //TODO: Send email
+    const resultEmail = await EmailController.sendEmail({
+      to: 'robertojf95@gmail.com',
+      subject: 'Test send email patters',
+      message: `teste message ${user.rememberToken}`,
+    });
+    console.log(resultEmail);
     return true;
   }
 }
